a1af0e9a31317e4573675880b65f9561fe461079
Lisa Sau <lisasau85@gmail.com>
2021-02-16 17:28:55 +0000

    Page Obeject Included, basUrl,
    Targeting Auth, Basket, Cypress.json

--- cypress/integration/SL/auth.spec.js
+++ cypress/integration/SL/auth.spec.js
@@ -1,17 +1,20 @@
 import * as _ from 'lodash'
+import Auth from '../Page_object/auth'
 
 describe('Log into Swag Labs', () => {
+    const auth = new Auth
     it('It Logs user in with available credentials', () => {
-        cy.visit(Cypress.env('url'))
-        cy.get('#user-name').type('standard_user')
-        cy.get('#password').type('secret_sauce')
-        cy.get('#login-button').click()
+        cy.visit(Cypress.env('home'))
+        auth.username().type('standard_user')
+        auth.password().type('secret_sauce')
+        auth.submitDetails().click()
     })
 })
 
 context('logout', () => {
+    const auth = new Auth
     it('Navigates to burger menu and clicks on logout', () => {
-        cy.get('.bm-burger-button').click()
-        cy.get('div .bm-menu-wrap > div > nav > #logout_sidebar_link').click()
+        auth.burgerMenu().click()
+        auth.logOutSlider().click()
     })
 })

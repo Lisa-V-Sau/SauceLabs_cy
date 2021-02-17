793002b967750ba049a3c79d6044a5ef826d832c
Lisa Sau <lisasau85@gmail.com>
2021-02-17 11:46:49 +0000

    Inventory PO
    Page Objects added to inventory.

--- cypress/integration/SL/inventory.spec.js
+++ cypress/integration/SL/inventory.spec.js
@@ -1,23 +1,27 @@
+/// <reference types ="Cypress"/>
 import { beforeEach } from 'mocha'
+import Inventory from '../Page_object/inventory'
+
+const inventory = new Inventory
 
 beforeEach( ()=> {
-    cy.visit('https://www.saucedemo.com/')
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
+    cy.visit(Cypress.env('home'))
+    inventory.username().type('standard_user')
+    inventory.password().type('secret_sauce')
+    inventory.submitDetails().click()
 })
 
 
 describe('To Check items on list', () => {
     it('returns total of items on list', () => {
-        cy.get('.inventory_container > div > div').eq(5)
+        inventory.container().eq(5)
     })
 })
 
 context('Select and purchase item', () => {
     it('Selects first and adds to basket', () => {
-        cy.get('.inventory_container > div > div:eq(0) > .pricebar > button').click()
-        cy.get('.inventory_container > div > div:eq(0) > .pricebar > button').then(($itemAdded) => {
+        inventory.addItemBtn().click()
+        inventory.itemAddedbtn().then(($itemAdded) => {
               const itemAddedtxt = $itemAdded.text()
               cy.log(itemAddedtxt)
               expect(itemAddedtxt).eq('REMOVE')
@@ -27,18 +31,18 @@ context('Select and purchase item', () => {
 
 context('Remove item from basket', () => {
     it('Clicks on remove button on item page', () => {
-        cy.get('.inventory_container > div > div:eq(0) > .pricebar > button').click()
+        inventory.rmvBtn().click()
     })
 })
 
 context('Check Title of item', () => {
     it('Check that item displays Sauce Labs Backpack', () => {
-        cy.get('.inventory_item_name:eq(0)').contains('Sauce Labs Backpack')
+        inventory.chckTitle().contains('Sauce Labs Backpack')
     })
 })
 
 context('Check Price of item', () => {
     it('Checks that the price of item is $29.99', () => {
-        cy.get('.inventory_item_price:eq(0)').contains('$29.99')
+        inventory.chckItemPrice().contains('29.99')
     })
 })

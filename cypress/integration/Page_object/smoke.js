df42fa0a999e34fbae149b6a49d3428953b0fe2e
Lisa Sau <lisasau85@gmail.com>
2021-03-08 22:14:49 +0000

    Smoke Test
    Smoke test which include eslint plugin configurations.

--- cypress/integration/Page_object/smoke.js
+++ cypress/integration/Page_object/smoke.js
@@ -1,10 +1,40 @@
-import Auth from './auth'
+/// <reference types ="Cypress"/>
+import Auth from './auth';
+import Inventory from './inventory';
 
-const auth = new Auth
+const auth = new Auth();
+const inventory = new Inventory();
 
 export const smokeTest = () => {
-    cy.visit(Cypress.env('home'))
-    auth.username().type('standard_user')
-    auth.password().type('secret_sauce')
-    auth.submitDetails().click()
+    // imported from tst_01: navigate to URL home
+    // Log in using credentials 
+    cy.visit(Cypress.env('home'));
+    auth.username().type('standard_user');
+    auth.password().type('secret_sauce');
+    auth.submitDetails().click();
+
+    // imported from tst_02: return number of items within inventory container
+    inventory.container().eq(5);
+
+    // import from tst_02: adds an item to basket and returns updated button display after item is added to basket
+    inventory.addItemBtn().click();
+    inventory.itemAddedbtn().then(($itemAdded) => {
+        const itemAddedtxt = $itemAdded.text();
+        cy.log(itemAddedtxt);
+        expect(itemAddedtxt).eq('REMOVE');
+    })
+
+    // imported from tst_02: removes item from basket
+    inventory.rmvBtn().click();
+
+    // imported from tst_02: checks the title of item
+    inventory.chckTitle().contains('Sauce Labs Backpack');
+
+    // imported from tst_02: checks the price of item
+    inventory.chckItemPrice().contains('29.99');
+
+    // imported from tst_01
+    // Logout: This is to be executed in the end of tests
+    auth.burgerMenu().click();
+    auth.logOutSlider().click();
 }

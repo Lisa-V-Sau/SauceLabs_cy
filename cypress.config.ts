901fbb242f643c5b2cd9f6d3e07f445e3e6e3cc3
lisasau85@gmail.com <lisasau85@gmail.com>
2024-10-17 14:56:53 +0100

    Cypress Updated

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  env: {
+    home: 'https://www.saucedemo.com/',
+    cart: 'https://www.saucedemo.com/cart.html',
+    chckoutStepOne: 'https://www.saucedemo.com/checkout-step-one.html',
+  },
+  chromeWebSecurity: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})

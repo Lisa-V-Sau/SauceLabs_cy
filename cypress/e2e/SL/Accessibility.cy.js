05736d90813d97e7e3f03abd5e6f158d102594f2
lisasau85@gmail.com <lisasau85@gmail.com>
2024-12-03 19:09:51 +0000

    Accessibility
    - Cypress Axe module plus core
    - Accessibility spec file
    - Accesibility tests including options

--- cypress/e2e/SL/Accessibility.cy.js
+++ cypress/e2e/SL/Accessibility.cy.js
@@ -1,9 +1,14 @@
+/// <reference types="cypress" />
+
 describe('Sauce Labs Demo Accessibility Testing', () => {
+    
     const su = 'standard_user'
     const lu = 'locked_out_user'
     const pw = 'secret_sauce'
+
     beforeEach(() => {
         cy.visit('https://www.saucedemo.com/v1/index.html')
+        // cy.injectAxe() must be called after cy.visit()
         cy.injectAxe()
     })
     afterEach(() => {
@@ -13,6 +18,26 @@ describe('Sauce Labs Demo Accessibility Testing', () => {
     })
 
     it('TC01: Login page accessbility testing', () => {
+        // cy.checkAlly() can be used only once cy.injectAxe() is called
+        // cy.checkAlly() also takes parameters
+        // The following parameters will allow for temporary by pass
+        cy.checkA11y(null, 
+            // {runOnly: {
+            //     type: 'tag',
+            //     values: ['wcag2a','wcag2aa', 'cat.*']
+            // },
+             rules: {
+                 'color-contrast': { enabled: true },
+                 'valid-lang': { enabled: true }
+             },
+             tags: {
+                 'wcag2a': { enabled: true },
+                 'wcag411': { enabled: true },
+                 'cat.parsing': { enabled: true },
+                 'cat.keyboard': { enabled: true },
+                 'best-practice': { enabled: true }
+             }}
+        )
         
     })
 })

6a97be737e72b1158a7ebdf1d3cc05d568396d01
Lisa Van Sau <lisasau85@gmail.com>
2024-12-09 21:52:44 +0000

    Refactor (#30)

--- cypress/e2e/SL/Accessibility.cy.js
+++ cypress/e2e/SL/Accessibility.cy.js
@@ -1,10 +1,14 @@
+/// <reference types="cypress" />
 
 describe('Sauce Labs Demo Accessibility Testing', () => {
+    
     const su = 'standard_user'
     const lu = 'locked_out_user'
     const pw = 'secret_sauce'
+
     beforeEach(() => {
         cy.visit('https://www.saucedemo.com/v1/index.html')
+        // cy.injectAxe() must be called after cy.visit()
         cy.injectAxe()
     })
     afterEach(() => {
@@ -18,14 +22,16 @@ describe('Sauce Labs Demo Accessibility Testing', () => {
         // cy.checkAlly() also takes parameters
         // The following parameters will allow for temporary by pass
         cy.checkA11y(null, 
-            // {runOnly: {
-            //     type: 'tag',
-            //     values: ['wcag2a','wcag2aa', 'cat.*']
-            // },
+            {runOnly: {
+                type: 'tag',
+                values: ['wcag2a','wcag2aa', 'cat.*']
+            },
              rules: {
                  'color-contrast': { enabled: true },
                  'valid-lang': { enabled: true }
              },
+             includedImpacts: 
+                ['low'],
              tags: {
                  'wcag2a': { enabled: true },
                  'wcag411': { enabled: true },
